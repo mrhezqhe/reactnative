33ac5afefcea0f859509c6d63c4cb8e462531c55
mrhezqhe <mrhez_qhe@yahoo.com>
2017-12-20 17:28:49 +0700

    first-commit
    initial commit

---
+++ albums/src/components/header.js
@@ -0,0 +1,11 @@
+//import library
+import React from 'react';
+import { Text } from 'react-native';
+
+//make Component
+const Header = () => {
+    return <Text>Albums</Text>;
+};
+
+//make Component export
+export default Header;
