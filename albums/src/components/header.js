de31c272a8240126528496e657582efe493e88c8
mrhezqhe <mrhez_qhe@yahoo.com>
2017-12-21 18:09:57 +0700

    update styling content
    get content from api, styling card, set state, class component, const function.

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,11 +1,31 @@
 //import library
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 //make Component
-const Header = () => {
-    return <Text>Albums</Text>;
+const Header = (props) => {
+    return (
+        <View style={styles.viewStyle}>
+          <Text style={styles.textStyle}>{props.textHeader}</Text>
+        </View>
+    );
 };
 
 //make Component export
 export default Header;
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 20 },
+    shadowOpacity: 0.2
+  },
+  textStyle: {
+    fontSize: 20
+  }
+});
